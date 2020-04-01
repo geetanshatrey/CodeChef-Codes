f2d5de6fa62d12718b0815a8708e6abeac7650b8
Geetansh Atrey <atrey.geetansh@gmail.com>
2020-04-01 21:23:31 +0530

    Basic Input Output in C++

---
+++ HackerRank/InputOutput.cpp
@@ -0,0 +1,12 @@
+//Link to the problem: https://www.hackerrank.com/challenges/cpp-input-and-output/problem
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<a+b+c;
+
+}
+
