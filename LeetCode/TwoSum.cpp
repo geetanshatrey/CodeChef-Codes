1163e6d3401938b384fc9bd88db536677a665c14
Geetansh Atrey <atrey.geetansh@gmail.com>
2020-04-03 00:05:04 +0530

    Problems with Links

---
+++ LeetCode/TwoSum.cpp
@@ -0,0 +1,18 @@
+// Link to the problem:  https://leetcode.com/problems/two-sum/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> arr;
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    arr.push_back(i);
+                    arr.push_back(j);
+                    return arr;
+                }
+            }
+        }
+        return arr;
+    }
+};
