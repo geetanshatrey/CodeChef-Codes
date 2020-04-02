f4aac42c49915601f0fb166120b41be9a4dd9e0f
Geetansh Atrey <atrey.geetansh@gmail.com>
2020-04-03 00:33:40 +0530

    Problem with Link

---
+++ Code Forces/Watermelon.cpp
@@ -0,0 +1,13 @@
+//Link to the problem: https://codeforces.com/contest/4/problem/A
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    if(n%2==0 && n>0 && n!=2)
+        cout<<"YES";
+    else
+        cout<<"NO";
+}
