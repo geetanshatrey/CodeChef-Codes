e5707b92dba2c229589ad4461aa4c4cb85a3fbd9
Geetansh Atrey <atrey.geetansh@gmail.com>
2020-04-02 22:29:19 +0530

    Basic Data Types in C++

---
+++ HackerRank/BasicDataTypes.cpp
@@ -0,0 +1,15 @@
+//Link to the problem: https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    long int b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<endl<<b<<endl<<c<<endl<<fixed<<setprecision(3)<<d<<endl<<fixed<<setprecision(9)<<e;
+}
