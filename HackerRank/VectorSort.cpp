1b51ffc81d1705438b4ee1e5daf138f0c530e60c
Geetansh Atrey <atrey.geetansh@gmail.com>
2020-04-13 23:49:29 +0530

    Problem with Link

---
+++ HackerRank/VectorSort.cpp
@@ -0,0 +1,25 @@
+//Link to the problem: https://www.hackerrank.com/challenges/vector-sort/problem
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    vector<int> s;
+    int n;
+    vector<int> :: iterator it; 
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int input;
+        cin>>input;
+        s.push_back(input);
+    }
+    sort(s.begin(),s.end());
+    for(it=s.begin();it!=s.end();it++)
+    {
+        cout<<*it<<" ";
+    }
+    return 0;
+}
