9ce683e4649517362bdad75d24e6a608b9d15477
Geetansh Atrey <atrey.geetansh@gmail.com>
2021-01-13 00:48:21 +0530

    Create ListComprehensions.py

---
+++ HackerRank/ListComprehensions.py
@@ -0,0 +1,9 @@
+# Link to the problem : https://www.hackerrank.com/challenges/list-comprehensions/problem
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    lists = [[a,b,c] for a in range(x+1) for b in range(y+1) for c in range(z+1) if a + b + c != n]
+    print(lists)
