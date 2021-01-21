ed874c001a9821b3d1798b0798f116ff04aa4bc2
Geetansh Atrey <atrey.geetansh@gmail.com>
2021-01-22 00:47:13 +0530

    Create Q21.cpp

---
+++ LeetCode/January Daily Challenge 2021/Q21.cpp
@@ -0,0 +1,23 @@
+// Link to the problem : 
+// Problem Name : Find the Most Competitive Subsequence
+
+class Solution {
+public:
+    vector<int> mostCompetitive(vector<int>& nums, int k) {
+        deque<int> queue;
+        int additionalCount = nums.size() - k;
+        for(int i = 0; i < nums.size(); i++) {
+            while (!queue.empty() && queue.back() > nums[i] && additionalCount > 0) {
+                queue.pop_back();
+                additionalCount--;
+            }
+            queue.push_back(nums[i]);
+        }
+        vector<int> result;
+        for (int i = 0; i < k; i++) {
+            result.push_back(queue.front());
+            queue.pop_front();
+        }
+        return result;
+    }
+};
